eded78fe5fa9f0abb2dfb1b2320acbd72b686343
LucasDomingos0103 <lucasantoniosd@yahoo.com.br>
2024-11-23 10:54:01 -0300

    teste

--- #3.py
+++ #3.py
@@ -15,15 +15,35 @@ d) o desempenho do carro (em Km/l, l/h e R$/Km).
 #horas para segundos = * 3600 - minutos para segundos = / 3600
 #horas para minutos ou minutos para segundos = * 60 - segundos para minutos ou minutos para horas = / 60
 
-iniciodapartida = float ( input ('Informe o momento da partida no formato h/min = '))
-chegada         = float ( input ('Informe o momento da chegada no formato h/min = '))
-descansoemseg   = float ( input ('Informe os segundos parados para descanso = '))
-combustivel     = float ( input ('O número de litros de combustível gasto = '))
-preçocombust    = float ( input ('Informe o preço do combustível em R$ = '))
-distancia       = float ( input ('Informe a distância percorrida em Km = '))
-
-tempviagem = round ((chegada - iniciodapartida) * 3600)
+from builtins import int
 
 
-print (f'{tempviagem}')
+partidahoras   = int ( input ('Informe a(s) hora(s) do momento da partida: '))
+partidamin     = int ( input ('Informe os minutos do momento da partida: '))
+chegadahoras   = int ( input ('Informe a(s) horas do momento da chegada: '))
+chegadamin     = int ( input ('Informe os minutos do momento da chegada: '))
+
+#tempo da viagem
+viagemhorasemseg = (chegadahoras - partidahoras) * 3600
+viagemminemseg   = (chegadamin - partidamin) * 60
+viagememseg      = viagemhorasemseg + viagemhorasemseg
+
+descansoemseg  = int ( input ('Informe os segundos parados para descanso = '))
+combustivel    = float ( input ('O número de litros de combustível gasto = '))
+preçocombust   = float ( input ('Informe o preço do combustível em R$ = '))
+distancia      = float ( input ('Informe a distância percorrida em Km = '))
+
+#velocidade média e velocidade em movimento em km/h
+velocidadglobal = distancia / (viagememseg / 3600)
+velocidadmov    = distancia / ((viagememseg - descansoemseg) / 3600)
+
+#custo da viagem
+custocombust    = combustivel * preçocombust
+
+
+
+
+print (f'{viagememseg}')
+print (f'{velocidadglobal}  {velocidadmov}')
+print (f'{custocombust}')
 
